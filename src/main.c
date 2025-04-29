1a11ad74be0b286e927b18655cafd7b4757691ec
Chadi Nadeif <2330029@collegebart.ca>
2025-04-29 13:58:12 -0400

    aancer

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #include <assert.h>
 #include "utils.h"
 
+#include "stb_image.h"
+#include "stb_image_write.h"
+
 #define HEAP_SIZE 2048 * 2048 * 4
 static uint8_t* heap = NULL;
 static size_t heap_top = 0;
@@ -15,8 +18,36 @@ void* allocate(size_t size) {
 	return &heap[old_top];
 }
 
+const char* img_names[] = {
+	"31.bmp",
+	"64.bmp",
+	"128.bmp"
+};
+
+const size_t img_name_len = sizeof(img_names) / sizeof(img_names[0]);
 int main(int argc, char** argv) {
 	heap = (uint8_t*)malloc(HEAP_SIZE);
 	assert(heap != NULL);
 	
+
+    int width, height, channels;
+	unsigned char* img = stbi_load("31.bmp", &width, &height, &channels, 0);
+	if (img == NULL) {
+		printf("Error in loading the image\n");
+		exit(1);
+	}
+	printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+	for (int i = 0; i < width * height * channels ; i += channels) {
+		unsigned char r = img[i];
+		unsigned char g = img[i+1];
+		unsigned char b = img[i+2];
+		if (r == 255) {
+			img[i + 1] = 0;
+			img[i + 2] = 0;
+		}
+	}
+
+	char filepath[_MAX_PATH] = { 0 };
+	snprintf(filepath, _MAX_PATH, "solution_%s", img_names[0]);
+	stbi_write_bmp(filepath, width, height, channels, img);
 }
